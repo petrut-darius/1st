bdc8e374c7050381d49a79493c4a4656e5c9db39
Petrut Darius-Ioan <131020605+petrut-darius@users.noreply.github.com>
2023-04-18 09:48:48 -0700

    Update raspuns.php

--- 17.04.2023/raspuns.php
+++ 17.04.2023/raspuns.php
@@ -1,4 +1,22 @@
 <?php
-    $var1 = $_POST['a'] + $_POST['b'];
-    echo $var1;
-        ?>
+
+ini_set('display_errors', 1);
+
+function sumanumere($a, $b)
+{
+    return $a + $b;
+}
+
+if (isset($_POST['a'])) {
+    $var1 = $_POST['a'];
+} else {
+    $var1 = 0;
+}
+
+if (isset($_POST['b'])) {
+    $var2 = $_POST['b'];
+} else {
+    $var2 = 0;
+}
+
+echo sumanumere($var1, $var2);
